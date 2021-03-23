9eea7070540cade3e4fd5ed90dd7b7c889188b9b
NethumL <nethumpinsara@gmail.com>
2021-03-23 20:11:29 +0530

    Fix error in notification string template
    Added a newline character to the end

--- src/main/java/com/nethum/emailclient/EmailStatRecorder.java
+++ src/main/java/com/nethum/emailclient/EmailStatRecorder.java
@@ -4,6 +4,6 @@ import java.time.LocalDate;
 
 public class EmailStatRecorder implements Observer {
     public void update(int number) {
-        System.out.format("%d notification(s) were received at %s", number, LocalDate.now().toString());
+        System.out.format("%d notification(s) were received at %s\n", number, LocalDate.now().toString());
     }
 }

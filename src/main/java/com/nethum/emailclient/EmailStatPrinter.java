7dee14cb2fc0cf2c361dbc1807f83060baf4f39b
NethumL <nethumpinsara@gmail.com>
2021-03-30 20:45:49 +0530

    Change datetime printing
    * Replaced LocalDate with LocalDateTime
    * Added a custom formatter for datetime

--- src/main/java/com/nethum/emailclient/EmailStatPrinter.java
+++ src/main/java/com/nethum/emailclient/EmailStatPrinter.java
@@ -2,11 +2,18 @@ package com.nethum.emailclient;
 
 import java.io.File;
 import java.io.IOException;
-import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class EmailStatPrinter implements Observer {
+    private static final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("uuuu/MM/dd h:m a");
+
     public void update(int number) {
-        String notification = String.format("%d notification(s) were received at %s", number, LocalDate.now().toString());
+        String notification = String.format(
+                "%d notification(s) were received at %s",
+                number,
+                LocalDateTime.now().format(dateTimeFormatter)
+        );
         File notificationsFile = new File("notifications.txt");
         new Thread(() -> {
             try {
--- src/main/java/com/nethum/emailclient/EmailStatRecorder.java
+++ src/main/java/com/nethum/emailclient/EmailStatRecorder.java
@@ -1,9 +1,16 @@
 package com.nethum.emailclient;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class EmailStatRecorder implements Observer {
+    private static final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("uuuu/MM/dd h:m a");
+
     public void update(int number) {
-        System.out.format("%d notification(s) were received at %s\n", number, LocalDate.now().toString());
+        System.out.format(
+                "%d notification(s) were received at %s\n",
+                number,
+                LocalDateTime.now().format(dateTimeFormatter)
+        );
     }
 }
